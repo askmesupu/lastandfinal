90f835945338105c598e17f9aa6a34d8aa3717b1
askmesupu <ask.me.pirated@gmail.com>
2025-09-16 01:28:39 +0600

    Create Hindi.js

---
+++ src/pages/Hindi.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Hindi() {
+  return (
+    <div className="page">
+      <h1>My Love in Hindi</h1>
+      <p>
+        Suhasini, teri muskaan mere jeevan ka ujala hai, jaise andheri raat mein
+        ek amar deepak jagata hai. Teri aankhon mein chhupa hai ek shant aur
+        gehra sa samundar, jisme doob jaane par har dard aur gham dhool ki tarah
+        udd jaata hai. Teri baaton ka jaadu mere hriday ke taaron ko chhu kar ek
+        madhur sangeet banata hai, aur har dhadkan bas tera naam japti rehti
+        hai. Suhasini, tu sirf ek naam nahi, ek roshni aur adhyatmikta ka
+        sansaar hai, jo mujhe har pal prem aur shanti ka anubhav karata hai.
+      </p>
+    </div>
+  );
+          }
